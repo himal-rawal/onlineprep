c927d03bb3293861438de1f279495993249f4858
Traitor00 <himalrawal07@gmail.com>
2023-07-28 09:41:02 +0545

    Add dropdown button to show category list in leaderboard page

--- lib/view/leaderboard_page.dart
+++ lib/view/leaderboard_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../res/widgets/leaderboard_widget/test_data_table.dart';
+import '../viewmodel/question_categories_viewmodel.dart';
+import '../viewmodel/score_viewmodel.dart';
 
 class LeaderBoardPage extends StatelessWidget {
   const LeaderBoardPage({super.key});
@@ -10,7 +13,44 @@ class LeaderBoardPage extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          _sliverAppBar(),
+          _sliverAppBar(context),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.all(30),
+              child: DropdownButton<String>(
+                //dropdownColor: const Color.fromARGB(255, 87, 172, 242),
+                value: context.watch<ScoreViewModel>().selectedDropdownValue,
+                icon: const Icon(Icons.arrow_drop_down),
+                iconSize: 30,
+                elevation: 16,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+                // underline: Container(
+                //   height: 2,
+                //   color: Colors.deepPurpleAccent,
+                // ),
+                onChanged: (String? newValue) {
+                  context
+                      .read<ScoreViewModel>()
+                      .setSelectedDropDownValue(newValue ?? '');
+                },
+                items: context
+                    .read<QuestionCategoriesViewModeel>()
+                    .nameOfCategories
+                    .map<DropdownMenuItem<String>>((value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      softWrap: false,
+                      maxLines: 1,
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
           const SliverToBoxAdapter(
             child: SimpleDataTable(),
           )
@@ -19,7 +59,7 @@ class LeaderBoardPage extends StatelessWidget {
     );
   }
 
-  SliverAppBar _sliverAppBar() {
+  SliverAppBar _sliverAppBar(BuildContext context) {
     return const SliverAppBar(
       title: Text(
         "Ranking",
@@ -31,7 +71,7 @@ class LeaderBoardPage extends StatelessWidget {
       floating: true,
       flexibleSpace: SizedBox(),
       expandedHeight: 80,
-      collapsedHeight: 60,
+      collapsedHeight: 65,
     );
   }
 }
