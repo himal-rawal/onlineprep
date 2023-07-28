7b8a720dc9faa4b9b046c327fd4aaa43194c1729
Traitor00 <himalrawal07@gmail.com>
2023-07-28 09:39:41 +0545

    Store received category data in array

--- lib/viewmodel/question_categories_viewmodel.dart
+++ lib/viewmodel/question_categories_viewmodel.dart
@@ -6,18 +6,18 @@ import '../repositories/question_categories_services.dart';
 /// Class To Fetch  Categories
 class QuestionCategoriesViewModeel with ChangeNotifier {
   List<Categories> _allCategories = [];
-  //List nameOfCategories = [];
+  List nameOfCategories = [];
   List<Categories> get allCategories => _allCategories;
   Future<List<Categories>> getAllCategories() async {
     try {
       final response = await QuestionCategoriesProvider.getQuestionCategories();
       _allCategories = response.categories ?? [];
       //Storing name of categories in List to use in DropDown Menu
-      // for (int i = 0; i < _allCategories.length; i++) {
-      //    if (!nameOfCategories.contains(_allCategories[i].title)) {
-      //      nameOfCategories.add(_allCategories[i].title);
-      //    }
-      // }
+      for (int i = 0; i < _allCategories.length; i++) {
+        if (!nameOfCategories.contains(_allCategories[i].title)) {
+          nameOfCategories.add(_allCategories[i].title);
+        }
+      }
     } catch (e) {
       rethrow;
     }
