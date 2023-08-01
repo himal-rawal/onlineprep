993669b84d279bfc0a5dd38d585926986b5f69a9
Traitor00 <himalrawal07@gmail.com>
2023-08-01 11:08:10 +0545

    Add url for score history

--- lib/res/app_url.dart
+++ lib/res/app_url.dart
@@ -1,12 +1,20 @@
 class AppUrl {
+  static const String baseUrl = "http://10.20.30.57:3001";
+
   String questionUrlMethod(String category) {
-    return "http://10.20.30.57:3001/api/v1/questions/?category=$category";
+    return "$baseUrl/api/v1/questions/?category=$category";
   }
 
-  static const String questionCategoriesUrl =
-      "http://10.20.30.57:3001/api/v1/categories";
-  static const String loginUrl = "http://10.20.30.57:3001/api/v1/auth/login";
-  static const String userdata = 'http://10.20.30.57:3001/api/v1/auth/me';
-  static const String signUP = "http://10.20.30.57:3001/api/v1/auth/register";
-  static const String score = "http://10.20.30.57:3001/api/v1/scores";
+  static const String questionCategoriesUrl = "$baseUrl/api/v1/categories";
+  static const String loginUrl = "$baseUrl/api/v1/auth/login";
+  static const String userdata = '$baseUrl/api/v1/auth/me';
+  static const String signUP = "$baseUrl/api/v1/auth/register";
+  static const String score = "$baseUrl/api/v1/scores";
+  String scoreRankingUrl(String category) {
+    return "$baseUrl/api/v1/scores/$category/category";
+  }
+
+  String scoreHistory(String userId) {
+    return "$baseUrl/api/v1/scores/$userId";
+  }
 }
